9ee5c332a26f2077e1abe598eeb2dd7625ce63e5
WillianKakihata <ra-22015763-2@alunos.unicesumar.edu.br>
2025-08-18 13:13:10 -0300

    chore: initialize project structure with base folders and configuration files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Route, Routes } from 'react-router'
+import './App.css'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route></Route>
+    </Routes>
+
+  )
+}
+
+export default App
